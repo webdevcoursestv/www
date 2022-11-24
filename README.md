f195508bde1482b50aee84cb26f4093fca9f7fe4
Chris Loggins <75412063+clogginsdev@users.noreply.github.com>
2022-11-24 09:53:13 -0600

    Restructured the files to match the Next Starter

--- README.md
+++ README.md
@@ -1,34 +1,46 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# DevmentorLive NextJS Starter Template
 
 ## Getting Started
 
-First, run the development server:
+To create an app using this starter
 
 ```bash
-npm run dev
-# or
-yarn dev
+npx create-next-app your-app-name -e https://github.com/devmentorlive-youtube/next-tailwind-starter
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## This template uses [TailwindCSS](https://tailwindcss.com/)!
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+## To run your server (development)
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
+```
+cd your-app-name
+npm run dev
+```
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+## Folder structure
 
-## Learn More
+```bash
+src
+├── features
+├── pages
+│   └── index.jsx
+├── ui
+└── index.jsx
+```
 
-To learn more about Next.js, take a look at the following resources:
+This app uses a modified/simplified version of [Atomic Design](https://bradfrost.com/blog/post/atomic-web-design/), better suited for modern web development.
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+### /ui
 
-## Deploy on Vercel
+This folder is for your "lego blocks", single purpose React components that can be combined together to make more complicated components, known as Features
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+### /features
+
+This folder is for more complex components, or features. Features are created by composing many UI components and usually, state
+
+
+### /pages
+
+This folder is for navigable components. A router automagically creates urls for anything in this folder. To create a page, combine features and ui components and put them in a component in this page. Typically state is managed in the feature, and any props that come from the server are passed through pages via the return object from getServerSideProps. Those props are then passed down into features as props, or put into a context.
---
+++ src/pages/index.jsx
@@ -0,0 +1,213 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import { wlist } from "../data/wishlist";
+import { eq } from "../data/equiptment";
+import { members } from "../data/gang";
+import { orders } from "../data/commands";
+
+export default function Home() {
+	const wishlist = wlist;
+	const equiptment = eq;
+	const gang = members;
+	const commands = orders;
+
+	const today = new Date();
+	const year = today.getFullYear();
+
+	return (
+		<div className={"container mx-auto px-4"}>
+			<Head>
+				<title>Mark's Gang - The Programmers Tribe</title>
+				<meta
+					name='description'
+					content='A place where we all hang out, code, and smoke weed. Chill vibes only.'
+				/>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<header className={"py-6 grid grid-cols-1 md:grid-cols-2 items-center"}>
+				<div className={"flex gap-4 items-center"}>
+					<div className={"logo text-3xl"}>
+						<span className={"font-bold text-white"}>Mark's</span>
+						<span className={"text-red-600 font-bold"}>Gang</span>
+					</div>
+					<p className={"text-white font-light italic"}>
+						a new web developer tribe!
+					</p>
+				</div>
+				<div className={"flex justify-center mt-6 md:mt-0 md:justify-end"}>
+					<button
+						className={
+							"bg-red-700 text-white p-3 rounded text-sm hover:bg-red-600"
+						}>
+						<a href='https://www.youtube.com/channel/UCnPaa-GMpyrmgU7TWX9AuEQ'>
+							Visit Our Tribe
+						</a>
+					</button>
+				</div>
+			</header>
+			<main>
+				<section className={"hero flex-col justify-center py-24"}>
+					<div className={"text-center"}>
+						<h1 className={"text-3xl md:text-5xl font-semibold mb-4"}>
+							Welcome to the Tribe!!!
+						</h1>
+						<p className={"text-md md:text-xl font-light"}>
+							Here at Mark's Gang everyone is welcome to come chill,
+							problem-solve, and smoke weed with us.<br></br> As long as you are
+							a coder of some kind!
+						</p>
+					</div>
+					<div className='mt-12'>
+						<iframe
+							className={"w-full h-[375px] md:w-[70%] md:h-[600px] mx-auto"}
+							src='https://www.youtube.com/embed/YxGug-swl8s'
+							title='YouTube video player'
+							frameBorder='0'
+							allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+							allowFullScreen></iframe>
+					</div>
+				</section>
+				<section className={"gang py-24"}>
+					<div className={"text-center"}>
+						<h2 className={"text-4xl font-semibold mb-4"}>Meet The Gang</h2>
+						<p className={"text-lg font-light"}>
+							These are the official members of Mark's Gang, they are some
+							execellent coders who you should speak to.
+						</p>
+					</div>
+					<div
+						className={
+							"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-12"
+						}>
+						{gang.map((member, index) => (
+							<div
+								key={index}
+								className={"bg-[#282828] rounded-lg shadow-lg p-4"}>
+								<div className={"flex flex-col items-center"}>
+									<div className={"w-24 h-24 rounded-full overflow-hidden"}>
+										<div className='rounded-full bg-red-800 w-[100px] h-[100px]'></div>
+									</div>
+									<h3 className={"text-xl font-semibold mt-4"}>
+										{member.name}
+									</h3>
+									<p
+										className={
+											"bg-red-700 p-[5px] text-white rounded text-sm my-2"
+										}>
+										{member.role}
+									</p>
+									<p className={"text-sm"}>{member.age} years old</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</section>
+				<section className={"commands py-24"}>
+					<div className={"text-center"}>
+						<h2 className={"text-4xl font-semibold mb-4"}>Commands</h2>
+						<p className={"text-lg font-light"}>
+							These are the commands of Mark's Gang channel and discord
+						</p>
+					</div>
+					<div
+						className={
+							"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-12"
+						}>
+						{commands.map((command, index) => (
+							<div
+								key={index}
+								className={"bg-[#282828] rounded-lg shadow-lg p-4"}>
+								<div className={"flex flex-col items-center"}>
+									<h3 className={"text-xl font-semibold mt-4"}>
+										{command.command}
+									</h3>
+									<p
+										className={
+											"bg-gray-700 p-[5px] text-white rounded text-sm my-2"
+										}>
+										{command.response}
+									</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</section>
+				<section className={"grid grid-cols-1 lg:grid-cols-2 gap-24 py-24"}>
+					<aside>
+						<h3 className={"text-2xl font-semibold mb-4"}>Mark's Wishlist</h3>
+						<div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+							{wishlist.map((wish, index) => (
+								<a key={index} href={wish.amazon_url}>
+									<div
+										className={
+											"bg-[#282828] hover:bg-[#313131] rounded-lg shadow-lg p-4 mb-4 text-center"
+										}>
+										<img
+											className={"mx-auto rounded-full"}
+											src={wish.img}
+											width={100}
+											height={100}
+										/>
+										<h3 className={"text-xl font-semibold mt-4"}>
+											{wish.item}
+										</h3>
+										<p
+											className={
+												"p-[5px] text-white text-sm my-2 font-semibold italic"
+											}>
+											{wish.price}
+										</p>
+									</div>
+								</a>
+							))}
+						</div>
+					</aside>
+					<aside>
+						<h3 className={"text-2xl font-semibold mb-4"}>Stream Equiptment</h3>
+						<div className={"grid grid-cols-1 md:grid-cols-2 gap-4"}>
+							{equiptment.map((eq, index) => (
+								<a key={index} href={eq.url}>
+									<div
+										className={
+											"min-h-[220px] bg-[#282828] hover:bg-[#313131] rounded-lg shadow-lg p-6 mb-4 text-center"
+										}>
+										<img
+											className={"mx-auto rounded-full"}
+											src={eq.img}
+											width={100}
+											height={100}
+										/>
+										<h3 className={"text-xl font-semibold mt-4"}>{eq.item}</h3>
+									</div>
+								</a>
+							))}
+						</div>
+					</aside>
+				</section>
+			</main>
+			<footer
+				className={
+					"flex flex-col  justify-between items-center md:flex-row bg-[#212121] text-white p-6 rounded"
+				}>
+				<div>
+					<p>
+						&copy; {year} <span className={"text-red-600"}>Mark Tellez</span>.
+						All rights reserved.{" "}
+					</p>
+				</div>
+
+				<div>
+					<p>
+						Developed by{" "}
+						<span className={"text-red-600"}>
+							<a href='https://www.kindlytip.com'>Chris Loggins</a>
+						</span>
+						.
+					</p>
+				</div>
+			</footer>
+		</div>
+	);
+}
